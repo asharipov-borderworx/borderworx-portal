718d2b753047e0d6c308a1bfeb5110f1115e7546
asharipov-borderworx <arthur.maratovich.dev@gmail.com>
2025-10-22 13:12:28 -0400

    Removed the arrow animation for buffalo culture page

--- src/app/buffalo-culture/page.tsx
+++ src/app/buffalo-culture/page.tsx
@@ -78,10 +78,7 @@ export default function BuffaloCulturePage() {
             </div>
           </div>
           
-          {/* Scroll Indicator */}
-          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-            <div className="w-6 h-6 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
-          </div>
+
         </section>
       </AnimationWrapper>
 
